c6be0f0866a36df1183f3b3818a42a0ff32bfc5c
Shubham Handa <shubham.handa@mavenwave.com>
2022-12-06 16:53:56 +0530

    crud-mysql

--- src/main/java/com/demo/envsql/model/Locker.java
+++ src/main/java/com/demo/envsql/model/Locker.java
@@ -3,6 +3,7 @@ package com.demo.envsql.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 
@@ -10,6 +11,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "locker")
 public class Locker {
+    @Id
     private Long id;
     private String owner;
 }
